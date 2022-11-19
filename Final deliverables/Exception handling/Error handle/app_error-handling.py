fc6c72d5d8df279badf84ed17e2e2a6361968450
Varunv63 <112741543+Varunv63@users.noreply.github.com>
2022-11-19 12:05:35 +0530

    Add files via upload

--- Final deliverables/Exception handling/Error handle/app_error-handling.py
+++ Final deliverables/Exception handling/Error handle/app_error-handling.py
@@ -3,24 +3,24 @@ from flask import Flask, request, jsonify, render_template
 import joblib
 import requests
 
+# IBM Cloud account Credentials.
+API_KEY = "S0ahhsqevpUY0Eu1YKv5Kyl38OMCy3haa5WCXw0am_wL"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey": API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
 app = Flask(__name__)
-model = joblib.load('Power_Prediction.sav')
-
-
+# model = joblib.load('Power_Prediction.sav')
 @app.route('/')
 def home():
-    return render_template('intro.html')
-
-
+    return render_template('index.html')
 @app.route('/predict')
 def predict():
     return render_template('predict.html')
-
-
 @app.route('/windapi',methods=['POST'])
 def windapi():
     city=request.form.get('city')
-    apikey="36c0639c05e38cd003ddea74d69b8822"
+    apikey="a802b0f626c637d04185e582b5ad0d58"
     url="http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apikey
     resp = requests.get(url)
     resp=resp.json()
@@ -31,31 +31,68 @@ def windapi():
     return render_template('predict.html', temp=temp, humid=humid, pressure=pressure,speed=speed)   
 @app.route('/y_predict',methods=['POST'])
 
-#File: 404.html, server location: 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html")
+from flask import jsonify, request
 
-# try/except logic that is a part of Python. If the try doesn't work, then we're catching all errors to the except statement,
-#So now we have a new file that we're referencing, 500.html. We pass one variable through, and that is the error.
-@app.route('/slashboard/')
-def slashboard():
-    try:
-        return render_template("dashboard.html", TOPIC_DICT = shamwow)
-    except Exception as e:
-	    return render_template("500.html", error = str(e))
-		
-        
+class InvalidAPIUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        super().__init__()
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+@app.errorhandler(InvalidAPIUsage)
+def invalid_api_usage(e):
+    return jsonify(e.to_dict()), e.status_code
+
+# an API app route for getting user information
+# a correct request might be /api/user?user_id=420
+@app.route("/api/user")
+def user_api(user_id):
+    user_id = request.arg.get("user_id")
+    if not user_id:
+        raise InvalidAPIUsage("No user id provided!")
+
+    user = get_user(user_id=user_id)
+    if not user:
+        raise InvalidAPIUsage("No such user!", status_code=404)
+
+    return jsonify(user.to_dict())
+    
 def y_predict():
     '''
     For rendering results on HTML GUI
     '''
     x_test = [[float(x) for x in request.form.values()]]
-    prediction = model.predict(x_test)
-    print(prediction)
-    output = prediction[0]
+    print(x_test)
+    # Deployed Link
+    payload_scoring = {"input_data": 
+			[{"field": [["Theoretical_Power", "Wind_Speed"]], 
+			"values": x_test}]}
+
+    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/373bfd45-d75f-4f2a-9c9c-a94decf0ec1f/predictions?version=2022-11-11', json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
+    print("Scoring response")
+    predictions =response_scoring.json()
+    print(predictions)
+    print('Final Prediction Result',predictions['predictions'][0]['values'][0][0])
+
+
+    pred =response_scoring.json()
+    print(pred)
+    #print('Final Prediction Result',predictions['predictions'][0]['values'][0][0])
+
+   # prediction = model.predict(x_test)
+    print(pred)
+    output = pred['predictions'][0]['values'][0][0]
     return render_template('predict.html', prediction_text='The energy predicted is {:.2f} KWh'.format(output))
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False, port=8080)
